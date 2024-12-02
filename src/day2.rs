692dc940653abbf3930615042708445de8d2cb74
Lukas Beierlieb <lukasbrlb.lb@gmail.com>
2024-12-02 11:46:58 +0100

    day 2 task 1

--- src/day2.rs
+++ src/day2.rs
@@ -1,7 +1,29 @@
-use std::iter::zip;
+use crate::helpers::parsing::numbers_per_line;
+
+fn check_diffs<F>(report: &[u64], check_function: F) -> bool
+where
+    F: Fn(i64) -> bool,
+{
+    report
+        .windows(2)
+        .map(|window| window[1] as i64 - window[0] as i64)
+        .all(check_function)
+}
+
+fn is_report_valid(report: &[u64]) -> bool {
+    let all_increasing = check_diffs(report, |diff| diff > 0);
+    let all_decreasing = check_diffs(report, |diff| diff < 0);
+    let small_steps = check_diffs(report, |diff| diff.abs() >= 1 && diff.abs() <= 3);
+
+    (all_increasing || all_decreasing) && small_steps
+}
 
 pub fn task_one(input: String) -> u64 {
-    0
+    let reports = numbers_per_line(&input);
+    reports
+        .into_iter()
+        .filter(|report| is_report_valid(report))
+        .count() as u64
 }
 
 pub fn task_two(input: String) -> u64 {
--- src/helpers/parsing.rs
+++ src/helpers/parsing.rs
@@ -7,3 +7,16 @@ pub fn numstring_to_numbers(input: &str) -> Vec<u64> {
         .filter_map(|cap| cap[0].parse::<u64>().ok())
         .collect()
 }
+
+pub fn numbers_per_line(input: &str) -> Vec<Vec<u64>> {
+    let re_num = Regex::new(r"[0-9]+").unwrap();
+    input
+        .lines()
+        .map(|line| {
+            re_num
+                .captures_iter(line)
+                .filter_map(|cap| cap[0].parse::<u64>().ok())
+                .collect()
+        })
+        .collect()
+}
