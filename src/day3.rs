4169af330f1ca2efbf1eb2e2a98e2ee6b1ab2221
Lukas Beierlieb <lukasbrlb.lb@gmail.com>
2024-12-03 12:52:25 +0100

    add part 2

--- src/day3.rs
+++ src/day3.rs
@@ -1,9 +1,40 @@
 use regex::Regex;
 
+#[derive(Debug)]
 enum Instruction {
     Do,
     Dont,
     MultiplyPair(u64, u64),
+}
+
+#[derive(Debug)]
+struct State {
+    enabled: bool,
+    sum: u64,
+}
+
+impl State {
+    fn initial() -> State {
+        State {
+            enabled: true,
+            sum: 0,
+        }
+    }
+
+    fn set_enabled(&self, val: bool) -> State {
+        State {
+            enabled: val,
+            sum: self.sum,
+        }
+    }
+
+    fn add_to_sum(&self, val: u64) -> State {
+        let add = if self.enabled { val } else { 0 };
+        State {
+            enabled: self.enabled,
+            sum: self.sum + add,
+        }
+    }
 }
 
 fn parse(input: &str) -> Vec<Instruction> {
@@ -35,5 +66,13 @@ pub fn task_one(input: String) -> u64 {
 }
 
 pub fn task_two(input: String) -> u64 {
-    0
+    let initial_state = State::initial();
+    parse(&input)
+        .into_iter()
+        .fold(initial_state, |state, ins| match ins {
+            Instruction::Do => state.set_enabled(true),
+            Instruction::Dont => state.set_enabled(false),
+            Instruction::MultiplyPair(a, b) => state.add_to_sum(a * b),
+        })
+        .sum
 }
