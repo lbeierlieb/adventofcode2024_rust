c8e11e27a7af5172a98c94f2e511434e1322265a
Lukas Beierlieb <lukasbrlb.lb@gmail.com>
2024-12-03 12:36:06 +0100

    rework part 1 to enable part 2

--- src/day3.rs
+++ src/day3.rs
@@ -1,20 +1,37 @@
 use regex::Regex;
 
-fn parse(input: &str) -> Vec<(u64, u64)> {
-    let re_num = Regex::new(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)").unwrap();
+enum Instruction {
+    Do,
+    Dont,
+    MultiplyPair(u64, u64),
+}
+
+fn parse(input: &str) -> Vec<Instruction> {
+    let re_num = Regex::new(r"do\(\)|don't\(\)|mul\(([0-9]{1,3}),([0-9]{1,3})\)").unwrap();
     re_num
         .captures_iter(input)
         .map(|cap| {
-            (
-                cap[1].parse::<u64>().unwrap(),
-                cap[2].parse::<u64>().unwrap(),
-            )
+            if cap[0].starts_with("don") {
+                Instruction::Dont
+            } else if cap[0].starts_with("do") {
+                Instruction::Do
+            } else {
+                let a = cap[1].parse::<u64>().unwrap();
+                let b = cap[2].parse::<u64>().unwrap();
+                Instruction::MultiplyPair(a, b)
+            }
         })
         .collect()
 }
 
 pub fn task_one(input: String) -> u64 {
-    parse(&input).into_iter().map(|(a, b)| a * b).sum()
+    parse(&input)
+        .into_iter()
+        .map(|ins| match ins {
+            Instruction::MultiplyPair(a, b) => a * b,
+            _ => 0,
+        })
+        .sum()
 }
 
 pub fn task_two(input: String) -> u64 {
