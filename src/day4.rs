31048fee7691cd6a183c2abd3e14d98b32e150ea
Lukas Beierlieb <lukasbrlb.lb@gmail.com>
2024-12-04 12:31:30 +0100

    parse input into grid struct

--- src/day4.rs
+++ src/day4.rs
@@ -1,4 +1,21 @@
+#[derive(Debug)]
+struct Grid {
+    data: Vec<Vec<char>>,
+}
+
+impl Grid {
+    fn parse(input: &str) -> Grid {
+        let data = input
+            .lines()
+            .map(|line| line.chars().collect::<Vec<_>>())
+            .collect::<Vec<_>>();
+        Grid { data }
+    }
+}
+
 pub fn task_one(input: String) -> u64 {
+    let grid = Grid::parse(&input);
+    dbg!(grid);
     0
 }
 
